54fe1e1e0e5e49beab276bb7db9ad8cf89ce1d8a
graphimatic <graphimatic@users.noreply.github.com>
2018-01-18 17:03:53 +0000

    setup firebase

--- Unit9_project/js/main.js
+++ Unit9_project/js/main.js
@@ -1,3 +1,18 @@
+
+
+
+  var config = {
+    apiKey: "AIzaSyBx4hqLgJcSmB19j490VwtyFvrYqJ-H4CU",
+    authDomain: "reservation-site-3111e.firebaseapp.com",
+    databaseURL: "https://reservation-site-3111e.firebaseio.com",
+    projectId: "reservation-site-3111e",
+    storageBucket: "reservation-site-3111e.appspot.com",
+    messagingSenderId: "627030577864"
+  };
+  firebase.initializeApp(config);
+
+
+
 function initMap() {
 
 	var map = new google.maps.Map(document.getElementById('map'), {
